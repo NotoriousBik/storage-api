aaeb0bd4c7bc40b24bd743a3564b1be90c37aed2
ilias_bikenov <ilias.bikenov@gmail.com>
2021-11-09 11:51:20 +0600

    Project linst clean

--- logger.js
+++ logger.js
@@ -1,89 +1,89 @@
-const Metadata = require('./models/Metadata')
-const fs = require("fs")
-const path = require("path")
+const Metadata = require('./models/Metadata');
+const fs = require('fs');
+const path = require('path');
 //write metadata to the db, rewrite if file exists
 const writeMetadata = async (name, mimeType, size) => {
 
-  //check if file is already exists
-  let metadata = await Metadata.findOne({ name: name })
+	//check if file is already exists
+	let metadata = await Metadata.findOne({ name: name });
   
-  if (!metadata) {
+	if (!metadata) {
 
-    try {
-      metadata = await Metadata.create({ name: name, mimeType: mimeType, size: size })
-      return (metadata);
-    } catch (e) {
-      console.log(e);
-      return e;
-    }
+		try {
+			metadata = await Metadata.create({ name: name, mimeType: mimeType, size: size });
+			return (metadata);
+		} catch (e) {
+			console.log(e);
+			return e;
+		}
 
-  } else {
+	} else {
 
-    try {
-      metadata = await await Metadata.findOneAndUpdate({ name: name }, { name: name, mimeType: mimeType, size: size }, { new: true, runValidators: true })
-      return (metadata);
-    } catch (e) {
-      console.log(e);
-      return e;
-    }
-  }
+		try {
+			metadata = await await Metadata.findOneAndUpdate({ name: name }, { name: name, mimeType: mimeType, size: size }, { new: true, runValidators: true });
+			return (metadata);
+		} catch (e) {
+			console.log(e);
+			return e;
+		}
+	}
 
-}
+};
 
 //get files metadata
 const getMetadata = async (name) => {
 
-  try {
-    const metadata = await Metadata.findOne({ name: name })
-    console.log(metadata);
-    if (!metadata) {
-      return { msg: 'File does not exist' }
-    }
+	try {
+		const metadata = await Metadata.findOne({ name: name });
+		console.log(metadata);
+		if (!metadata) {
+			return { msg: 'File does not exist' };
+		}
 
-    return (metadata);
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
+		return (metadata);
+	} catch (e) {
+		console.log(e);
+		return e;
+	}
 
-}
+};
 
 //get files metadata
 const checkFolderSize = async (dirPath) => {
 
-  //function which returns all files in a folder
-  const getAllFiles = function(dirPath) {
-    files = fs.readdirSync(dirPath)
+	//function which returns all files in a folder
+	const getAllFiles = function(dirPath) {
+		files = fs.readdirSync(dirPath);
   
-    arrayOfFiles = []
+		arrayOfFiles = [];
   
-    files.forEach(function(file) {
-      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-        arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-      } else {
-        arrayOfFiles.push(path.join(__dirname, dirPath, file))
-      }
-    })
+		files.forEach(function(file) {
+			if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+				arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);
+			} else {
+				arrayOfFiles.push(path.join(__dirname, dirPath, file));
+			}
+		});
   
-    return arrayOfFiles
-  }
+		return arrayOfFiles;
+	};
   
-  //function which summarize all files sizes in bytes
-  const getTotalSize = function(dirPath) {
-    const arrayOfFiles = getAllFiles(dirPath)
+	//function which summarize all files sizes in bytes
+	const getTotalSize = function(dirPath) {
+		const arrayOfFiles = getAllFiles(dirPath);
   
-    let totalSize = 0
+		let totalSize = 0;
   
-    arrayOfFiles.forEach(function(filePath) {
-      totalSize += fs.statSync(filePath).size
-    })
+		arrayOfFiles.forEach(function(filePath) {
+			totalSize += fs.statSync(filePath).size;
+		});
   
-    return totalSize
-  }
+		return totalSize;
+	};
 
-  return (getTotalSize(dirPath))
+	return (getTotalSize(dirPath));
 
-}
+};
 
-module.exports = { writeMetadata, getMetadata, checkFolderSize}
+module.exports = { writeMetadata, getMetadata, checkFolderSize};
 
--- models/Metadata.js
+++ models/Metadata.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const MetadataSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'name writing is failed'],
-        trim: true
-    },
-    mimeType: {
-        type: String,
-        required: [true, 'mimeType writing is failed'],
-    },
-    size: {
-        type: Number,
-        required: [true, 'size writing is failed'],
-    }
-})
+	name: {
+		type: String,
+		required: [true, 'name writing is failed'],
+		trim: true
+	},
+	mimeType: {
+		type: String,
+		required: [true, 'mimeType writing is failed'],
+	},
+	size: {
+		type: Number,
+		required: [true, 'size writing is failed'],
+	}
+});
 
-module.exports = mongoose.model('Metadata', MetadataSchema)
+module.exports = mongoose.model('Metadata', MetadataSchema);
